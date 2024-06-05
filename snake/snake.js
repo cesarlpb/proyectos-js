b18de553ec205313360cd3c4dc20ee6542c33785
jponsgar <jponsgar@gmail.com>
2024-06-05 10:48:36 +0200

    Cambio Diseño

--- snake/snake.js
+++ snake/snake.js
@@ -13,7 +13,7 @@ let dy = 0;
 let manzana = {x: Math.floor(Math.random() * 20) * cuadrado, y: Math.floor(Math.random() * 20) * cuadrado};
 let juego;
 let puntos = 0;
-ctx.fillStyle = "gray";
+ctx.fillStyle = "rgba(100,255,100,0.8)";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 let selectorVeloz = document.getElementById('veloz');
 
